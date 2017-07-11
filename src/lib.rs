06fe046d6f03a00d52ba9971b7aeb2aa1a09e3b7
Thomas Gideon <cmdln@thecommandline.net>
2017-07-11 18:21:04 -0400

    Add background tiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate ai_behavior;
 extern crate find_folder;
 extern crate gfx_device_gl;
+extern crate graphics;
 extern crate piston_window;
 extern crate sprite;
 extern crate uuid;
@@ -8,6 +9,8 @@ extern crate uuid;
 mod object;
 
 use gfx_device_gl::Resources;
+use graphics::Image;
+use graphics::rectangle::square;
 use piston_window::*;
 use object::Object;
 use sprite::*;
@@ -53,8 +56,21 @@ impl Game {
     }
 
     pub fn on_draw(&mut self, e: &Input, _: RenderArgs, w: &mut PistonWindow) {
+        let assets = find_folder::Search::ParentsThenKids(3, 3)
+            .for_folder("assets")
+            .unwrap();
+        let image = Image::new().rect(square(0.0, 0.0, 640.0));
+        let bg = Texture::from_path(&mut w.factory,
+                                       assets.join("bg.png"),
+                                       Flip::None,
+                                       &TextureSettings::new()).unwrap();
         w.draw_2d(e, |c, g| {
             clear([1.0, 1.0, 1.0, 1.0], g);
+            for number in 0..100 {
+                let x: f64 = (number % 10 * 64).into();
+                let y: f64 = (number / 10 * 64).into();
+                image.draw(&bg, &Default::default(), c.transform.trans(x, y).zoom(0.1), g);
+            }
             self.scene.draw(c.transform, g);
         });
     }
